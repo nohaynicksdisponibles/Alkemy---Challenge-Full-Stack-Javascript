39c5144dc2535bb032430a3e49a7ecc554d3197e
nohaynicksdisponibles <jonathanezequielsosa@hotmail.com>
2021-07-17 20:54:16 -0300

    pantallas login y signup finalizadas

---
+++ client/src/screens/Balance.jsx
@@ -0,0 +1,24 @@
+function Balance(){
+    return(
+        <>
+            <nav>
+
+            </nav>
+
+            <div>
+                 <div>
+                    {/*aca va el balance*/}
+                </div>
+
+                <div>
+                    {/*aca va el dropdown*/}
+                </div>
+                <div>
+                    {/*aca van las ultimas operaciones*/}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Balance;
